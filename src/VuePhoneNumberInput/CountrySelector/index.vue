8ba48b7b69a6a40e7248e943908c1271d7d6e9f6
Devessier <baptiste@devessier.fr>
2021-09-13 19:43:16 +0200

    chore: use relative path to import scss files

--- src/VuePhoneNumberInput/CountrySelector/index.vue
+++ src/VuePhoneNumberInput/CountrySelector/index.vue
@@ -292,7 +292,7 @@
 </script>
 
 <style lang="scss" scoped>
-  @import '@/assets/scss/variables.scss';
+  @import '../../assets/scss/variables.scss';
   @import 'style-helpers';
   @import './assets/iti-flags/flags.css';
 
--- src/VuePhoneNumberInput/InputTel/index.vue
+++ src/VuePhoneNumberInput/InputTel/index.vue
@@ -154,7 +154,7 @@
 </script>
 
 <style lang="scss" scoped>
-  @import '@/assets/scss/variables';
+  @import '../../assets/scss/variables.scss';
   @import 'style-helpers';
 
   .input-tel {
